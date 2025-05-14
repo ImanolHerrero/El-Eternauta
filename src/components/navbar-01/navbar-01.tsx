8cdbea6d565efff8aa4b6bb91ddac7d2631dd4fc
Imanol <imano.arias@gmail.com>
2025-05-14 07:46:10 -0300

    navbar agregado

---
+++ src/components/navbar-01/navbar-01.tsx
@@ -0,0 +1,28 @@
+import { Logo } from "./logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+
+const Navbar01Page = () => {
+  return (
+    <nav className="h-16 bg-background border-b">
+      <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
+        {/* Left: Logo */}
+        <div className="flex-shrink-0">
+          <Logo />
+        </div>
+
+        {/* Center: NavMenu (only visible on md and up) */}
+        <div className="hidden md:flex flex-1 justify-center">
+          <NavMenu />
+        </div>
+
+        {/* Right: NavigationSheet (only visible on mobile) */}
+        <div className="flex-shrink-0 md:hidden">
+          <NavigationSheet />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar01Page;
